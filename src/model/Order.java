65f247f9cad0c4d7ac751abf5dd6b55ca3669302
ashik <ashik@studevs>
2016-12-09 03:48:35 +0600

    Added Order.java model for adding food items in order list.

---
+++ src/model/Order.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order implements Serializable{
+    
+    private final int tableNumber ;
+    private final Map<String, Integer> foodItems ;
+    private int finalBill ;
+
+    public Order(int tableNumber) {
+        this.tableNumber = tableNumber;
+        this.foodItems = new HashMap<>();
+        this.finalBill = 0;
+    }
+    
+    
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public Map<String, Integer> getFoodItems() {
+        return foodItems;
+    }
+
+    public int getFinalBill() {
+        return finalBill;
+    }
+}
