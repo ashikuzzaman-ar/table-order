55c6693c176980252d8462b4c4c668e6bb013d67
Ashif Rahaman <ashifrahamanuis@gmail.com>
2016-12-09 00:51:06 +0600

    Designed the main menu. Edited the method's name.

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,41 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package core;
 
 import java.util.Scanner;
 
-/**
- *
- * @author ashif
- */
+
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int choice = 0;
+        int choice;
         do {
             //print menu and read choice
             menu();
             choice = in.nextInt();
             switch (choice) {
                 case 1:
-                    printAllAuthors();
+                    showFoodItems();
                     break;
                 case 2:
-                    printAllTitlesForAuthor();
+                    order();
                     break;
                 case 3:
-                    printAllAuthorsForTitle();
+                    showOrderedList();
                     break;
                 case 4:
-                    printAllAuthorsRanking();
+                    deleteOrderList();
                     break;
                 case 5:
-                    printAllAuthorsRankingFromTitle();
+                    finalizeOrder();
                     break;
                 case 6:
                     System.out.println("Thank you for using our System.");
@@ -45,7 +38,7 @@ public class Main {
                     throw new AssertionError();
             }
         } while (choice != 6);
-        
+
         System.exit(0);
     }
 
@@ -53,54 +46,51 @@ public class Main {
      * print menu
      */
     private static void menu() {
-        System.out.println("");
-        System.out.println("###########################################################");
-        System.out.println("Enter valid choice: ");
-        System.out.println("1. Select All Authors");
-        System.out.println("2. Select All Titles for Author");
-        System.out.println("3. Select All Authors for Title");
-        System.out.println("4. Author's Ranking");
-        System.out.println("5. Author's Ranking by Book Title");
-        System.out.println("6. Exit");
-        System.out.print(">>>>> ");
+        System.out.println("---------------------------------");
+        System.out.println("|        Main Menu              |");
+        System.out.println("|   1. Show Food Menu           |");
+        System.out.println("|   2. Order Food               |");
+        System.out.println("|   3. Show Ordered List        |");
+        System.out.println("|   4. Delete Item Form List    |");
+        System.out.println("|   5. Finalize Order           |");
+        System.out.println("|   6. Exit                     |");
+        System.out.println("---------------------------------");
+        System.out.print("Please, Enter your choice: ");
     }
 
     /**
-     * Print all authors
+     * Print all food items menu
      */
-    private static void printAllAuthors() {
+    private static void showFoodItems() {
 
         //TODO: Have to write logic here
     }
 
     /**
-     * Print all authors for title
+     * Order food items
      */
-    private static void printAllAuthorsForTitle() {
+    private static void order() {
 
         //TODO: Have to write logic here
     }
 
     /**
-     * Print all titles for authors
+     * Print food ordered list
      */
-    private static void printAllTitlesForAuthor() {
+    private static void showOrderedList() {
         //TODO: Have to write logic here
     }
 
     /**
-     * Print all author's ranking respect to published books
+     * Delete items from the ordered list
      */
-    private static void printAllAuthorsRanking() {
+    private static void deleteOrderList() {
 
         //TODO: Have to write logic here
     }
 
-    /**
-     * Print all author's ranking respect to popularity
-     */
-    private static void printAllAuthorsRankingFromTitle() {
-
+    private static void finalizeOrder() {
+    
         //TODO: Have to write logic here
     }
 }
